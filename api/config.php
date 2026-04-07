187235c590c2f92e2571d0cc10bd5f42caf88b28
auxmarketingpax-stack <auxmarketingpax@gmail.com>
2026-04-07 14:39:14 -0300

    CRM 02

--- api/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST','localhost');
-define('DB_NAME','crm');
-define('DB_USER','root');
-define('DB_PASS','');
-?>
--- api/leads/list.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../db.php';
-require_once '../security.php';
-auth();
-
-echo json_encode(db()->query("SELECT * FROM leads")->fetchAll(PDO::FETCH_ASSOC));
-?>
